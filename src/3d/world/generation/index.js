f819871e7f4cdcb38d8ce2bae36fe12fbf5e1399
Michael Ryan <mryanwebdev@gmail.com>
2025-08-11 17:31:52 -0500

    feat: add generator registry

---
+++ src/3d/world/generation/index.js
@@ -0,0 +1,30 @@
+import { createHexGenerator } from './HexWorldGenerator';
+
+// Registry of available world generator factories keyed by version name
+const registry = {
+  hex: createHexGenerator,
+};
+
+/**
+ * Return a list of available generator version keys.
+ */
+export function availableWorldGenerators() {
+  return Object.keys(registry);
+}
+
+/**
+ * Create a world generator for a given version and seed.
+ * Falls back to the default 'hex' generator when the version is unknown.
+ */
+export function createWorldGenerator(version = 'hex', seed) {
+  const factory = registry[version] || registry.hex;
+  return factory(seed);
+}
+
+/**
+ * Allow external registration of additional generator versions at runtime.
+ * Useful for experimentation with new algorithms.
+ */
+export function registerWorldGenerator(version, factory) {
+  if (version && typeof factory === 'function') registry[version] = factory;
+}
