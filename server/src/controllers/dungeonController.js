0cf2a67e2d010000e2c4693c0627f257efa20e07
Michael Ryan <mryanwebdev@gmail.com>
2025-09-24 09:56:12 -0500

    Refactor location service to use unified world location type

--- server/src/controllers/dungeonController.js
+++ server/src/controllers/dungeonController.js
@@ -1,8 +1,14 @@
-import { listDungeons, getDungeon, createDungeon } from '../services/locationService.js';
+import {
+  listLocationsByType,
+  getLocationByType,
+  createLocationOfType,
+} from '../services/locationService.js';
+
+const DUNGEON_TYPE = 'DUNGEON';
 
 export const index = async (req, res) => {
   try {
-    const items = await listDungeons();
+    const items = await listLocationsByType(DUNGEON_TYPE);
     res.json(items);
   } catch (e) {
     console.error(e);
@@ -12,7 +18,7 @@ export const index = async (req, res) => {
 
 export const show = async (req, res) => {
   try {
-    const item = await getDungeon(req.params.id);
+    const item = await getLocationByType(req.params.id, DUNGEON_TYPE);
     if (!item) {
       res.status(404).send('Not Found');
       return;
@@ -26,8 +32,7 @@ export const show = async (req, res) => {
 
 export const create = async (req, res) => {
   try {
-    const { worldLocationId, difficulty, maxDepth } = req.body;
-    const created = await createDungeon(worldLocationId, { difficulty, maxDepth });
+    const created = await createLocationOfType(DUNGEON_TYPE, req.body ?? {});
     res.status(201).json(created);
   } catch (e) {
     console.error(e);
--- server/src/controllers/townController.js
+++ server/src/controllers/townController.js
@@ -1,8 +1,14 @@
-import { listTowns, getTown, createTown } from '../services/locationService.js';
+import {
+  listLocationsByType,
+  getLocationByType,
+  createLocationOfType,
+} from '../services/locationService.js';
+
+const TOWN_TYPE = 'TOWN';
 
 export const index = async (req, res) => {
   try {
-    const items = await listTowns();
+    const items = await listLocationsByType(TOWN_TYPE);
     res.json(items);
   } catch (e) {
     console.error(e);
@@ -12,7 +18,7 @@ export const index = async (req, res) => {
 
 export const show = async (req, res) => {
   try {
-    const item = await getTown(req.params.id);
+    const item = await getLocationByType(req.params.id, TOWN_TYPE);
     if (!item) {
       res.status(404).send('Not Found');
       return;
@@ -26,8 +32,7 @@ export const show = async (req, res) => {
 
 export const create = async (req, res) => {
   try {
-    const { worldLocationId, population, factionId } = req.body;
-    const created = await createTown(worldLocationId, { population, factionId });
+    const created = await createLocationOfType(TOWN_TYPE, req.body ?? {});
     res.status(201).json(created);
   } catch (e) {
     console.error(e);
