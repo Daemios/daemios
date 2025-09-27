a01dde49d73f51e7e9a44b64bae9cde62a4e44de
Michael Ryan <mryanwebdev@gmail.com>
2025-09-27 18:10:57 -0500

    refactor: route app through module routers

---
+++ server/src/modules/user/index.ts
@@ -0,0 +1,6 @@
+export { userRouter } from './user.routes';
+export { userController } from './user.controller';
+export { userService } from './user.service';
+export { userRepository } from './user.repository';
+export * from './user.domain';
+export * from './user.validators';
--- server/src/types/globals.d.ts
+++ server/src/types/globals.d.ts
@@ -6,39 +6,3 @@ declare module 'express-session' {
   }
 }
 
-declare module './world/locations' {
-  import { Router } from 'express';
-  const value: Router;
-  export default value;
-}
-
-declare module './world/towns' {
-  import { Router } from 'express';
-  const value: Router;
-  export default value;
-}
-
-declare module './world/dungeons' {
-  import { Router } from 'express';
-  const value: Router;
-  export default value;
-}
-
-// Top-level module names used in some imports
-declare module 'src/routes/world/locations' {
-  import { Router } from 'express';
-  const value: Router;
-  export default value;
-}
-
-declare module 'src/routes/world/towns' {
-  import { Router } from 'express';
-  const value: Router;
-  export default value;
-}
-
-declare module 'src/routes/world/dungeons' {
-  import { Router } from 'express';
-  const value: Router;
-  export default value;
-}
